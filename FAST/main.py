6094aa84901b7d0001f481e1d7fa3ff4ef0dd3b9
Dreamerless <122043750@upq.edu.mx>
2025-02-13 16:50:25 -0600

    Lista para almacenar tareas

--- FAST/main.py
+++ FAST/main.py
@@ -1,78 +1,54 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI,HTTPException
 from typing import Optional
 
-app= FastAPI(
-    title= 'Mi primerAPI 192',
-    description= 'Gabriel Manzano Ruíz',
-    version= '1.0.1'
+app = FastAPI(
+    title='mi primer api',
+    description= 'Acosta Gonzalez Maximiliano',
+    version='1.0.1'
 )
 
-usuarios=[
-    {"id": 1,"nombre":"ivan", "edad":37},
-    {"id": 2, "nombre":"isay", "edad":15},
-    {"id": 3, "nombre":"luis", "edad":18},
-    {"id": 4, "nombre":"ana", "edad":37}
+usuarios = [
+    {"id": 1, "nombre": "Alfredo", "edad": 21},
+    {"id": 2, "nombre": "Polo", "edad": 22},
+    {"id": 3, "nombre": "Pedro", "edad": 19},  
+    {"id": 4, "nombre": "Maria", "edad": 20}
 ]
 
-#Endponit home
+#endPoint home
 
-@app.get('/', tags=['Mi calificación tai api'])
+@app.get('/')
 def home():
-    return {'messaje':'Hello World'}
+    return {'hello':'world FastAPI'}
 
-#Endpoint CONSULTA TODOS
-@app.get('/todosUsuarios', tags =['Operaciones CRUD'])
+#endpoint consulta todos
+@app.get('/todosUsuarios', tags=['operaciones CRUD'])
 def leerUsuarios():
-    return{"Los usuarios registrados son ":usuarios}
+    return {"Los usuarios registrados son: ": usuarios}
 
-#Endpoint Agregar Nuevos Usuarios
-@app.post('/ususario/', tags=['Operaciones CRUD'])
+#endpoint Añadir
+@app.post('/addUsuarios/', tags=['operaciones CRUD'])
 def agregarUsuario(usuario:dict):
-        for usr in usuarios:
-            if usr["id"] == usuario.get("id"):
-                raise HTTPExeption(status_code=400, detail="El id ya esxiste")
+    for usr in usuarios:
+        if usr["id"]== usuario.get("id"):
+            raise HTTPException(status_code=400, detail="Id existente")
+    usuarios.append(usuario)
+    return usuario
 
-                usuarios.append(usuario)
-                return usuario
-                
-#Endpoint parámetro obligatorio
-@app.get('/usuario/{id}', tags=['Parámetro Obligatorio id'])
-def consultaUsuario(id: int):
-    #Simulación de base de datos
-    #consulta = select * from usuario where id = id
-   return{'Se encontro el usuario con el id':id}
+#endpoint actualizar
+@app.put('/actualizarUsuarios/{id}', tags=['operaciones CRUD'])
+def actualizarUsuario(id:int, usuario:dict):
+    for index,usr in enumerate(usuarios):
+        if usr["id"]== id:
+                usuarios[index].update(usuario)
+                return usuarios(index)
+    raise HTTPException(status_code=400, detail="Id inexistente")
 
+# Endpoint para eliminar un usuario
+@app.delete("/eliminarUsuarios/{id}", tags=["operaciones CRUD"])
+def eliminar_usuario(id: int):
+    for index, usr in enumerate(usuarios):
+        if usr.id == id:
+            del usuarios[index]
+            return {"message": "Usuario eliminado correctamente"}
 
-@app.get("/usuario/", tags=["Parámetro Opcional"])
-def consultaUsuario2(id: Optional[int] = None):
-
-    if id is not None:
-        for usu in usuarios:
-            if usu['id'] == id:
-                return {"mensaje":"Usuario encontrado", "usuario:": usu}
-        return {"mensaje":f"No se encontro el usuario con id: {id}"}
-    else:
-        return {"mensaje":"No se proporciono un id"}
-
-
-#endpoint con varios parametro opcionales
-@app.get("/usuarios/", tags=["3 parámetros opcionales"])
-async def consulta_usuarios(
-    id: Optional[int] = None,
-    nombre: Optional[str] = None,
-    edad: Optional[int] = None
-):
-    resultados = []
-
-    for usuario in usuarios:
-        if (
-            (id is None or usuario["id"] == id) and
-            (nombre is None or usuario["nombre"].lower() == nombre.lower()) and
-            (edad is None or usuario["edad"] == edad)
-        ):
-            resultados.append(usuario)
-
-    if resultados:
-        return {"usuarios_encontrados": resultados}
-    else:
-        return {"mensaje": "No se encontraron usuarios que coincidan con los parámetros proporcionados."}
+    raise HTTPException(status_code=404, detail="Usuario no encontrado")
