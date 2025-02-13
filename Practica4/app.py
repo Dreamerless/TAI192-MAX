73f4119ddea6155b4aec7c7980a0492f9d89d3e6
Dreamerless <122043750@upq.edu.mx>
2025-02-13 16:58:41 -0600

    Actualizar tarea

--- Practica4/app.py
+++ Practica4/app.py
@@ -33,4 +33,11 @@ def agregar_tarea(tarea: dict):
     tareas.append(tarea)
     return tarea
 
-    
+    # Endpoint actualizar tarea
+@app.put('/actualizarTarea/{id}', tags=['operaciones CRUD'])
+def actualizar_tarea(id: int, tarea_actualizada: dict):
+    for index, t in enumerate(tareas):
+        if t["id"] == id:
+            tareas[index].update(tarea_actualizada)
+            return tareas[index]
+    raise HTTPException(status_code=400, detail="ID inexistente")
